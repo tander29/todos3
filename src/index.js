e974d56dbe748f024b38d53560e0a49a73bec4f6
Travis Anderson <thetanderson@gmail.com>
2018-08-13 15:49:05 -0400

    mvp

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Index = () => (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+    </BrowserRouter>
+)
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+registerServiceWorker();
